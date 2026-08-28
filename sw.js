0fad498b80e87bcfa1924df9f4f954b754e834db
Zlonrav <58839698+Zlonrav@users.noreply.github.com>
2026-08-28 21:37:17 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gen-v1.451'; // Меняйте версию при обновлении стилей или JS
+const CACHE_NAME = 'gen-v1.452'; // Меняйте версию при обновлении стилей или JS
 const ASSETS = [
   './',
   'index.html',
