af8a530722a6d027d5dc64968bbbfe00baf94aa8
Zlonrav <58839698+Zlonrav@users.noreply.github.com>
2026-09-02 09:11:01 +0300

    Update hashmaster.js

--- hashmaster.js
+++ hashmaster.js
@@ -122,7 +122,7 @@
         game.attempts = 0;
     
         document.body.classList.add('hash-master-active');
-        btn.textContent = 'ВЗЛОМАТЬ ХЭШ';
+        btn.textContent = 'ВЗЛОМАТЬ';
     
         s1.value = ''; s2.value = ''; num.value = '';
         window._currentW1 = ''; window._currentW2 = ''; window._currentNum = '';
